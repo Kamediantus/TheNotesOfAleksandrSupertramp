fef941ad42a9dc7c62577436f78faed7367ba0c5
Aleksandr Rodichev <a.rodichev@fundcount.com>
2022-02-27 01:41:51 +0300

    raw add store

--- webBlog/src/main/java/ru/rodichev/webBlog/controller/StoreController.java
+++ webBlog/src/main/java/ru/rodichev/webBlog/controller/StoreController.java
@@ -2,9 +2,11 @@ package ru.rodichev.webBlog.controller;
 
 import java.util.*;
 
+import org.json.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 import ru.rodichev.webBlog.entity.*;
 import ru.rodichev.webBlog.repo.*;
@@ -22,5 +24,17 @@ public class StoreController {
     @GetMapping("/stores")
     public ResponseEntity<List<Store>> getAllStores() {
         return new ResponseEntity<>(storeRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/addStore")
+    public ResponseEntity<List<Product>> addStore(@RequestBody String rawProduct, Model model) {
+        JSONObject json = new JSONObject(rawProduct);
+        Store store = new Store();
+        store.setTitle(json.getString("title"));
+        store.setDiscount(json.getDouble("discount"));
+        if (storeRepository.save(store) != null) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 }
